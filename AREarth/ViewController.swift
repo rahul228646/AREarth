cad202e1aa13f475d5de84aa7ea8c575577c4c2f
rahul228646 <rahulkaushik228646@gmail.com>
2022-10-24 13:04:21 +0530

    anchors added

--- AREarth/ViewController.swift
+++ AREarth/ViewController.swift
@@ -25,20 +25,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        // Set the scene to the view
 //        sceneView.scene = scene
         
-        let sphere = SCNSphere(radius: 0.3)
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/earth.jpg")
-        sphere.materials = [material]
+
         
-        let rotateAction = SCNAction.rotateBy(x: 0, y: 0.25, z: 0, duration: 2.0)
+//        let rotateAction = SCNAction.rotateBy(x: 0, y: 0.25, z: 0, duration: 2.0)
+//
+//        // node
+//        let node = SCNNode()
+//        node.position = SCNVector3(x: 0, y: 0, z: -0.8)
         
-        // node
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 0, z: -0.8)
-        node.geometry = sphere
-        node.runAction(SCNAction.repeatForever(rotateAction))
+//        node.runAction(SCNAction.repeatForever(rotateAction))
         
-        sceneView.scene.rootNode.addChildNode(node)
+//        sceneView.scene.rootNode.addChildNode(node)
         
         sceneView.automaticallyUpdatesLighting = true
     }
@@ -48,9 +45,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
+
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -58,6 +60,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//        guard let planeAnchor = anchor as? ARPlaneAnchor else {return }
+//        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+//        let gridMaterial = SCNMaterial()
+//        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+//        plane.materials = [gridMaterial]
+//
+//        let planeNode = SCNNode()
+//
+//        planeNode.geometry = plane
+//        planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+//        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+//
+//        node.addChildNode(planeNode)
+//    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+                
+                let sphere = SCNSphere(radius: 0.02)
+                let material = SCNMaterial()
+                material.diffuse.contents = UIImage(named: "art.scnassets/earth.jpg")
+                sphere.materials = [material]
+                
+                let node = SCNNode()
+                node.position = SCNVector3(
+                                            x: hitResult.worldTransform.columns.3.x,
+                                            y: hitResult.worldTransform.columns.3.y + node.boundingSphere.radius*2,
+                                            z: hitResult.worldTransform.columns.3.z
+                                          )
+                node.geometry = sphere
+                sceneView.scene.rootNode.addChildNode(node)
+                
+                let rotateAction = SCNAction.rotateBy(x: 0, y: 0.25, z: 0, duration: 2.0)
+                node.runAction(SCNAction.repeatForever(rotateAction))
+                
+            }
+        }
     }
 
 
